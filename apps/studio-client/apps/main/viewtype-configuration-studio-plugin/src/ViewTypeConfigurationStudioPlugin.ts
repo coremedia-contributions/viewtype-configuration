ef37d2a166bc8289d6af8ec527d8f1a83a244697
Philipp Hofmann <philipp.hofmann@coremedia.com>
2022-06-16 15:57:09 +0200

    Moved config into struct

--- apps/studio-client/apps/main/viewtype-configuration-studio-plugin/src/ViewTypeConfigurationStudioPlugin.ts
+++ apps/studio-client/apps/main/viewtype-configuration-studio-plugin/src/ViewTypeConfigurationStudioPlugin.ts
@@ -1,11 +1,16 @@
 import ViewTypeSelectorForm
   from "@coremedia-blueprint/studio-client.main.blueprint-forms/forms/containers/ViewTypeSelectorForm";
+import ContentTypes_properties from "@coremedia/studio-client.cap-base-models/content/ContentTypes_properties";
 import AddItemsPlugin from "@coremedia/studio-client.ext.ui-components/plugins/AddItemsPlugin";
 
+import CopyResourceBundleProperties
+  from "@coremedia/studio-client.main.editor-components/configuration/CopyResourceBundleProperties";
 import StudioPlugin from "@coremedia/studio-client.main.editor-components/configuration/StudioPlugin";
 
 import Config from "@jangaroo/runtime/Config";
 import ConfigUtils from "@jangaroo/runtime/ConfigUtils";
+import resourceManager from "@jangaroo/runtime/l10n/resourceManager";
+import ViewTypeConfiguration_properties from "./ViewTypeConfiguration_properties";
 import StoriesSliderOptionsForm from "./configuration/StoriesSliderOptionsForm";
 import TetrisBlockOptionsForm from "./configuration/TetrisBlockOptionsForm";
 
@@ -29,6 +34,12 @@ class ViewTypeConfigurationStudioPlugin extends StudioPlugin {
                 ],
               }),
             ],
+          }),
+        ],
+        configuration: [
+          new CopyResourceBundleProperties({
+            destination: resourceManager.getResourceBundle(null, ContentTypes_properties),
+            source: resourceManager.getResourceBundle(null, ViewTypeConfiguration_properties),
           }),
         ],
       }), config);
--- apps/studio-client/apps/main/viewtype-configuration-studio-plugin/src/ViewTypeConfiguration_properties.ts
+++ apps/studio-client/apps/main/viewtype-configuration-studio-plugin/src/ViewTypeConfiguration_properties.ts
@@ -1,15 +1,60 @@
+
 /**
  * Interface values for ResourceBundle "ViewTypeConfiguration".
  * @see ViewTypeConfiguration_properties#INSTANCE
  */
 interface ViewTypeConfiguration_properties {
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnDesktop_text": string;
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnDesktop_toolTip": string;
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnDesktop_emptyText": string;
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnMobile_text": string;
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnMobile_toolTip": string;
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnMobile_emptyText": string;
+  "CMCollection_localSettings.viewtypeConfiguration.containerBackgroundColor_text": string;
+  "CMCollection_localSettings.viewtypeConfiguration.containerBackgroundColor_toolTip": string;
+  "CMCollection_localSettings.viewtypeConfiguration.containerBackgroundColor_emptyText": string;
+  "CMCollection_localSettings.viewtypeConfiguration.textColor_text": string;
+  "CMCollection_localSettings.viewtypeConfiguration.textColor_toolTip": string;
+  "CMCollection_localSettings.viewtypeConfiguration.textColor_emptyText": string;
 
+  TetrisBlock_i1: string
+  TetrisBlock_i2: string
+  TetrisBlock_j: string
+  TetrisBlock_l: string
+  TetrisBlock_o: string
+  TetrisBlock_s: string
+  TetrisBlock_t1: string
+  TetrisBlock_t2: string
+  TetrisBlock_z: string
 }
 
 /**
  * Singleton for the current user Locale's instance of ResourceBundle "ViewTypeConfiguration".
  * @see ViewTypeConfiguration_properties
  */
-const ViewTypeConfiguration_properties: ViewTypeConfiguration_properties = {  };
+const ViewTypeConfiguration_properties: ViewTypeConfiguration_properties = {
+  "CMCollection_localSettings.viewtypeConfiguration.containerBackgroundColor_text": "Container Background Color",
+  "CMCollection_localSettings.viewtypeConfiguration.containerBackgroundColor_toolTip": "Container Background Color",
+  "CMCollection_localSettings.viewtypeConfiguration.containerBackgroundColor_emptyText": "Enter the container background color here.",
+  "CMCollection_localSettings.viewtypeConfiguration.textColor_text": "Text Color",
+  "CMCollection_localSettings.viewtypeConfiguration.textColor_toolTip": "Text Color",
+  "CMCollection_localSettings.viewtypeConfiguration.textColor_emptyText": "Enter the text color here.",
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnDesktop_text": "Items (Desktop)",
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnDesktop_toolTip": "Number of items to show on Desktop",
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnDesktop_emptyText": "8",
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnMobile_text": "Items (Mobile)",
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnMobile_toolTip": "Number of items to show on Mobile",
+  "CMCollection_localSettings.viewtypeConfiguration.itemsToShowOnMobile_emptyText": "4",
+
+  TetrisBlock_i1: "Shape I (left)",
+  TetrisBlock_i2: "Shape I (right)",
+  TetrisBlock_j: "Shape J",
+  TetrisBlock_l: "Shape L",
+  TetrisBlock_o: "Shape O",
+  TetrisBlock_s: "Shape S",
+  TetrisBlock_t1: "Shape T (left)",
+  TetrisBlock_t2: "Shape T (right)",
+  TetrisBlock_z: "Shape Z",
+};
 
 export default ViewTypeConfiguration_properties;
